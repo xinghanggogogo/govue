8d89238709e58cc51f55438fa37721f9249b4f49
xinghang <xinghang@thunder.com.cn>
2017-01-03 16:42:44 +0800

    vue-cli实例

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,30 @@
 <template>
 	<div id="gogogo">
 		<ul>
-            <li><router-link to="/test">test</router-link></li>
-            <li><router-link to="/test2">test2</router-link></li>
-			<li><router-link to="/test2">test3</router-link></li>
+                    <li><router-link to="/test">test</router-link></li>
+                    <li><router-link to="/test2">test2</router-link></li>
+			 <li><router-link to="/test2">test3</router-link></li>
    		</ul>
 			<router-view></router-view>
 		<Header></Header>
 		<Footer></Footer>
+		<div class="xinghagn"></div>
+		<div class="xinghang">test</div>
 	</div>
 </template>
 
 <script>
-
 import Header from '/home/xinghang/gogogo/src/components/header.vue';
 import Footer from '/home/xinghang/gogogo/src/components/footer.vue';
-
 export default {
 	name: 'app',
 	data () {
 		return {
 		}
-	},
-	methods(){
-
-	}
+	},	
 	components: {
 		Header: Header,
 		Footer: Footer
-
 	}
 }
 </script>
@@ -37,5 +33,8 @@ export default {
 	ul {
 		list-style: none;
 	}
-
+	div {
+		border-width: 1px;
+	}
+	
 </style>
