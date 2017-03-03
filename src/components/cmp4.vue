c9064373bc821b23448ff1302863deb23ecc1eaa
xinghang <xinghang@thunder.com.cn>
2017-03-03 16:18:01 +0800

    update

--- src/components/cmp4.vue
+++ src/components/cmp4.vue
@@ -1,21 +1,15 @@
 <template>
     <div>
-        <p>父组件</p>
-        <button v-on:click="clickParent">点击</button>
-        <cmp5 ref="child"></cmp5>
+        <p>子组件</p>
     </div>
 </template>
 
 <script>
-    import cmp5 from './cmp5.vue';
     export default {
-        name: "parent",
-        components: {
-            cmp5
-        },
+        name: "cmp4",
         methods: {
-            clickParent() {
-                this.$refs.child.handleParentClick("父组件调用子组件方法.");
+            handleParentClick(e) {
+                console.info(e)
             }
         }
     }
--- src/page/page3.vue
+++ src/page/page3.vue
@@ -1,20 +1,22 @@
 <template>
 	<div class="page3">
 		<p>父组件调用子组件方法</p>
-        <cmp4></cmp4>
+		<button v-on:click="clickParent">点击</button>
+		<childcmp ref="child"></childcmp>
 	</div>
 </template>
 
 <script>
-import cmp4 from '../components/cmp4.vue'
+import childcmp from '../components/cmp4.vue'
 export default {
-	name: 'page3',
-	data () {
-		return {
-		}
-	},
+	name: "parent",
 	components: {
-		cmp4
+		childcmp
+	},
+	methods: {
+		clickParent() {
+			this.$refs.child.handleParentClick("父组件调用子组件方法.");
+		}
 	}
 }
 </script>
